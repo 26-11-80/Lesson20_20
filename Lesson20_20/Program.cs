8e26556055c10e9adafb9406bcc9878ff9529b88
anvarovagulnaz <anvarovagulnaz@gmail.com>
2022-01-04 22:46:00 +0500

    Добавьте файлы проекта.

---
+++ Lesson20_20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20_20
+{
+    class Program
+    {
+        delegate double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Lenght;
+            double L = myDelegate(5);
+            Console.WriteLine(L);
+          
+
+            myDelegate = Area;
+            double A = myDelegate(5);
+            Console.WriteLine(A);
+           
+
+
+            myDelegate = Volume;
+            double V = myDelegate(5);
+            Console.WriteLine(V);
+            Console.ReadKey();
+
+        }
+        static double Lenght(double R) => 2 * Math.PI * R;
+        static double Area(double R) => Math.PI * R * R;
+        static double Volume(double R) => Math.PI * 4 / 3 * Math.Pow(R, 3);
+    }
+}
